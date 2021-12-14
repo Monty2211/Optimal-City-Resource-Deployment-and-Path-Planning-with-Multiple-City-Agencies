d9c81f5a6d8f62972d41b92fd7dfeb60a1043da5
RochelleAllan <rochelleaallan22@gmail.com>
2021-12-14 15:01:17 -0500

    clean up-- folder structure

---
+++ Police-Patrols/py_files/basemap-roc.py
@@ -0,0 +1,41 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import pandas as pd
+
+lats, lons,= [],[]
+with open('Shootings.csv') as csvfile:
+    reader = csv.DictReader(csvfile,delimiter=',')
+    for data in reader:
+        lats.append(float(data['Latitude']))
+        lons.append(float(data['Longitude']))
+
+
+# How much to zoom from coordinates (in degrees)
+zoom_scale = 0
+
+# Setup the bounding box for the zoom and bounds of the map
+bbox = [np.min(lats)-zoom_scale,np.max(lats)+zoom_scale,\
+        np.min(lons)-zoom_scale,np.max(lons)+zoom_scale]
+
+plt.figure(figsize=(12,6))
+# Define the projection, scale, the corners of the map, and the resolution.
+m = Basemap(projection='merc',llcrnrlat=bbox[0],urcrnrlat=bbox[1],\
+            llcrnrlon=bbox[2],urcrnrlon=bbox[3],lat_ts=10,resolution='h')
+
+# Draw coastlines and fill continents and water with color
+m.drawcoastlines()
+m.fillcontinents(color='yellow',lake_color='aqua')
+
+# draw parallels, meridians, and color boundaries
+m.drawparallels(np.arange(bbox[0],bbox[1],(bbox[1]-bbox[0])/5),labels=[1,0,0,0])
+m.drawmeridians(np.arange(bbox[2],bbox[3],(bbox[3]-bbox[2])/5),labels=[0,0,0,1],rotation=45)
+m.drawmapboundary(fill_color='darkblue')
+
+# build and plot coordinates onto map
+x,y = m(lons,lats)
+m.plot(x,y,'r*',markersize=5)
+plt.title("Rochester ")
+plt.savefig('ROCHESTER.png', format='png', dpi=500)
+plt.show()
